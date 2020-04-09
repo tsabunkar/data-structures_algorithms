01ca7b63c41b9325d4e86fd1fcc8634860dcf9ec
tsabunkar <tsabunkar@gmail.com>
2020-04-09 19:40:39 +0530

    day 9 problem finished with quadratic TC

---
+++ leetcode/30-days-challenge/day-9.js
@@ -0,0 +1,52 @@
+/**
+ * Given two strings S and T, return if they are equal when both are typed
+ * into empty text editors. # means a backspace character.
+ *
+ *
+ * Input: S = "ab#c", T = "ad#c" ==> Output: true
+ * Explanation: Both S and T become "ac".
+ *
+ * Input: S = "ab##", T = "c#d#" ==> Output: true
+ *  Explanation: Both S and T become "".
+ *
+ * Input: S = "a##c", T = "#a#c" ==> Output: true
+ * Explanation: Both S and T become "c".
+ *
+ * Input: S = "a#c", T = "b" ==> Output: false
+ * Explanation: S becomes "c" while T becomes "b".
+ *
+ * ! solve it in O(N) time and O(1) space
+ */
+
+/**
+ * @param {string} S
+ * @param {string} T
+ * @return {boolean}
+ */
+var backspaceCompare = function (S, T) {
+  let StrS = removeBackspaceCharacter([...S]);
+  let StrT = removeBackspaceCharacter([...T]);
+
+  return StrS === StrT ? true : false;
+};
+
+function removeBackspaceCharacter(charArray) {
+  for (let i = 0; i < charArray.length; i++) {
+    if (charArray[i] === '#') {
+      charArray[i] = charArray[i - 1] = null;
+      i = i - 2;
+    }
+    charArray = charArray.filter((ele) => ele);
+  }
+
+  console.log(charArray);
+  return charArray.join('');
+}
+
+// console.log(backspaceCompare('ab#c', 'ad#c'));
+// console.log(backspaceCompare('ab##', 'c#d#'));
+console.log(backspaceCompare('a##c', '#a#c'));
+// console.log(backspaceCompare('a#c', 'b'));
+// console.log(backspaceCompare('xywrrmp', 'xywrrmu#p'));
+
+// console.log(backspaceCompare('a##c', '#a#c'));
