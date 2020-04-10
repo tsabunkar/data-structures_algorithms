4f584ca686157e12ac9fe6112d447bfb8ea16029
tsabunkar <tsabunkar@gmail.com>
2020-04-10 17:19:04 +0530

    day 10 problem solved

--- leetcode/30-days-challenge/day-5.js
+++ leetcode/30-days-challenge/day-5.js
@@ -1,5 +1,5 @@
 /**
- * Best Time to Buy and Sell Stock II
+ * !Best Time to Buy and Sell Stock II
  *
  * Say you have an array for which the ith element is the price of a given stock on day i.
  *
@@ -11,4 +11,26 @@
  *
  */
 
-var maxProfit = function (prices) {};
+/* var maxProfit = function (prices) {
+  if (prices == null || prices.length == 0) return 0;
+  let hold = 0;
+  let have = false;
+  let earn = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    let money = prices[i + 1] - prices[i];
+    if (money > 0) {
+      if (!have) {
+        hold = prices[i];
+        have = true;
+      }
+    } else if (money < 0) {
+      if (have) {
+        earn += prices[i] - hold;
+        have = false;
+      }
+    }
+  }
+  if (have) earn += prices[prices.length - 1] - hold;
+
+  return earn;
+}; */
--- leetcode/30-days-challenge/day-9.js
+++ leetcode/30-days-challenge/day-9.js
@@ -1,4 +1,7 @@
 /**
+ *
+ * ! Backspace String Compare
+ *
  * Given two strings S and T, return if they are equal when both are typed
  * into empty text editors. # means a backspace character.
  *
