8129474c94f1175dda34e89ef847834d60151c6d
tsabunkar <tsabunkar@gmail.com>
2021-05-17 20:30:55 +0530

    sol4 using for loop of complete string

--- leetcode/algo/easy/defanging-an-ip-address.js
+++ leetcode/algo/easy/defanging-an-ip-address.js
@@ -28,7 +28,17 @@ var defangIPaddr3 = function (address) {
   return address.replace(/\./g, "[.]");
 };
 
+var defangIPaddr4 = function (address) {
+  let result = "";
+  // for of on string will iterate over all the characters
+  for (const char of address) {
+    if (char === ".") result += "[.]";
+    else result += char;
+  }
+  return result;
+};
+
 //console.log(defangIPaddr("1.1.1.1"));
 //console.log(defangIPaddr("255.100.50.0"));
 //console.log(defangIPaddr2("255.100.50.0"));
-console.log(defangIPaddr3("255.100.50.0"));
+console.log(defangIPaddr4("255.100.50.0"));
